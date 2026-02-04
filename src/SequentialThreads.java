d5ba3f707c31a70aa2ae0dc2b9b42710c81e2bdd
майор <ucebydla910@gmail.com>
2026-02-04 12:14:22 +0500

    Update SequentialThreads.java

--- src/SequentialThreads.java
+++ src/SequentialThreads.java
@@ -1,4 +1,34 @@
-package PACKAGE_NAME;
-
 public class SequentialThreads {
+    public static void main(String[] args) {
+        Thread firstThread = new Thread(() -> {
+            for (int i = 1; i <= 5; i++) {
+                System.out.println("Первый поток: Сообщение " + i);
+                try {
+                    Thread.sleep(300);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        Thread secondThread = new Thread(() -> {
+            System.out.println("Второй поток начинает работу после первого");
+            for (int i = 1; i <= 3; i++) {
+                System.out.println("Второй поток: Действие " + i);
+                try {
+                    Thread.sleep(300);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        firstThread.start();
+        try {
+            firstThread.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        secondThread.start();
+    }
 }

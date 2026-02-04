324cb8b4b47ef786702ef437f44d48c658a58e0b
майор <ucebydla910@gmail.com>
2026-02-04 12:13:34 +0500

    Update NameThread.java

--- src/NameThread.java
+++ src/NameThread.java
@@ -1,4 +1,19 @@
-package PACKAGE_NAME;
+class NamedThread extends Thread {
+    public NamedThread(String name) {
+        super(name);
+    }
 
-public class NameThread {
+    @Override
+    public void run() {
+        System.out.println("Поток " + getName() + " начинает работу");
+        for (int i = 1; i <= 4; i++) {
+            System.out.println("Поток " + getName() + ": выполнение " + i);
+            try {
+                Thread.sleep(400);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        System.out.println("Поток " + getName() + " завершил работу");
+    }
 }

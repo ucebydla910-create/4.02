8e64a4d75a3221f1e7b814751edfa37800fa1952
майор <ucebydla910@gmail.com>
2026-02-04 12:14:48 +0500

    Update ppp.java

--- src/ppp.java
+++ src/ppp.java
@@ -1,4 +1,14 @@
-package PACKAGE_NAME;
-
-public class ppp {
+class RunnableTask implements Runnable {
+    @Override
+    public void run() {
+        for (int i = 1; i <= 5; i++) {
+            System.out.println("Задача в потоке " + Thread.currentThread().getName() +
+                    ": Шаг " + i);
+            try {
+                Thread.sleep(300);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
 }

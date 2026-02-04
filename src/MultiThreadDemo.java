25b28551a2f42db55b10921a61dff7e2e6bb23f3
майор <ucebydla910@gmail.com>
2026-02-04 12:13:08 +0500

    Update MultiThreadDemo.java

--- src/MultiThreadDemo.java
+++ src/MultiThreadDemo.java
@@ -1,4 +1,27 @@
-package PACKAGE_NAME;
-
 public class MultiThreadDemo {
+    public static void main(String[] args) {
+        // Поток через наследование
+        SimpleThread thread1 = new SimpleThread();
+
+        // Потоки через Runnable
+        Thread thread2 = new Thread(new RunnableTask(), "Thread-B");
+        Thread thread3 = new Thread(() -> {
+            for (int i = 1; i <= 5; i++) {
+                System.out.println("Поток C: Работа " + i);
+                try {
+                    Thread.sleep(600);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }, "Thread-C");
+
+        // Переименуем первый поток для наглядности
+        thread1.setName("Thread-A");
+
+        // Запускаем все потоки
+        thread1.start();
+        thread2.start();
+        thread3.start();
+    }
 }

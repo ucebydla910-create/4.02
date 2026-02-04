5450e61e8f185c7b797854cef4b12fa6500b28f8
майор <ucebydla910@gmail.com>
2026-02-04 12:15:16 +0500

    Update run.java

--- src/run.java
+++ src/run.java
@@ -1,4 +1,14 @@
-package PACKAGE_NAME;
-
-public class run {
+class SimpleThread extends Thread {
+    @Override
+    public void run() {
+        for (int i = 1; i <= 5; i++) {
+            System.out.println("Поток " + Thread.currentThread().getName() +
+                    ": Итерация " + i);
+            try {
+                Thread.sleep(500);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
 }
